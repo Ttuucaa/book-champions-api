5143996f6723ec6c45af0e1f6e10641e9b06cfee
Ttuucaa <tomaslattuca@gmail.com>
2025-09-03 21:07:34 -0300

    actualizacion
    fetch useeffect agregado

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import { port } from "./config.js";
 import bookRoutes from "./routes/books.routes.js";
 import { sequelize } from "./db.js";
 import { Book } from "./models/Book.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors()); // <-- habilita CORS para todas las rutas
 app.use(express.json()); // <-- importante para leer JSON
 
 try {
   await sequelize.sync(); // <-- primero sincroniza la base de datos
-  app.use(express.json()); // <-- middleware para parsear JSON
+  app.use(express.json());
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+  }); 
   app.use("/books", bookRoutes); // <-- usa prefijo
   app.listen(port, () => {
     console.log(`Servidor escuchando en puerto ${port}`);
